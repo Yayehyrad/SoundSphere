68fec78ff96df01515ee24f2326b860e6b4a03c2
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-10-16 20:31:42 +0300

    completed signup page ui

--- lib/core/configs/themes/app_theme.dart
+++ lib/core/configs/themes/app_theme.dart
@@ -10,6 +10,10 @@ class AppTheme {
     inputDecorationTheme: InputDecorationTheme(
         filled : true ,
         fillColor : Colors.transparent ,
+          hintStyle: const TextStyle(
+          color: Color.fromARGB(255, 61, 58, 58),
+          fontWeight: FontWeight.w500
+        ),
         contentPadding : EdgeInsets.all(30),
         enabledBorder : OutlineInputBorder(
           borderRadius : BorderRadius.circular(30),
@@ -48,17 +52,21 @@ class AppTheme {
         filled : true ,
         fillColor : Colors.transparent ,
         contentPadding : EdgeInsets.all(30),
+        hintStyle: const TextStyle(
+          color: Color.fromARGB(255, 184, 173, 173),
+          fontWeight: FontWeight.w500
+        ),
         enabledBorder : OutlineInputBorder(
           borderRadius : BorderRadius.circular(30),
           borderSide: const BorderSide(
-            color: Colors.black,
+            color: Colors.white,
             width: 0.4
           )
         ) ,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
           borderSide: const BorderSide(
-            color: Colors.black,
+            color: Colors.white,
             width: 0.4
           )
         )
--- lib/presentation/pages/signup.dart
+++ lib/presentation/pages/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:soundsphere/common/widgets/appbar/basic_app_bar.dart';
+import 'package:soundsphere/common/widgets/button/basic_app_btn.dart';
 import 'package:soundsphere/core/configs/assets/app_assets.dart';
 
 class Signup extends StatelessWidget {
@@ -9,11 +10,25 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BasicAppBar( text: Image.asset(AppAssets.logo , height: 120 , width: 120,),),
-      body: Column(
-        children: [ 
-          
-
-        ],
+      bottomNavigationBar: _signinText(context),
+      body: 
+      
+      Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [ 
+            _registerText(),
+            SizedBox(height: 50,),
+            _fullNameField(context),
+            SizedBox(height: 20,),
+            _emailField(context),
+            SizedBox(height: 20,),
+            _passwordField(context),
+            SizedBox(height: 20,),
+            BasicAppBtn(onPressed: (){}, title: "Create Account")
+        
+          ],
+        ),
       ),
     );
   }
@@ -25,7 +40,43 @@ class Signup extends StatelessWidget {
     ) , textAlign  : TextAlign.center);
   }
 
-  Widget _fullNameField(){
-    return TextField();
+  Widget _fullNameField(BuildContext context){
+    return TextField(
+      decoration  : InputDecoration(
+        hintText : 'Full Name'
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme
+      )
+    );
+  }
+
+  Widget _emailField(BuildContext context){
+    return TextField(
+      decoration: InputDecoration(hintText: "Email").applyDefaults(
+        Theme.of(context).inputDecorationTheme
+      ),
+    );
+  }
+  Widget _passwordField(BuildContext context){
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Password" 
+      ).applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
+  Widget _signinText(BuildContext context) {
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Do you have an account ?" ,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14
+          ),
+        ) , 
+        TextButton(onPressed: (){}, child: Text("sign in"))
+      ],
+    );
   }
 }
